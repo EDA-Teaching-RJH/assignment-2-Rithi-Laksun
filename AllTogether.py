b440852af59dd567f5667cae9962349a0b6c40dd
Rithi-Laksun <rl471@kent.ac.uk>
2024-10-24 19:58:27 +0000

    done while loop and list manipulation

--- AllTogether.py
+++ AllTogether.py
@@ -8,16 +8,4 @@ for i in range(n):
     number_list.append(numbers)
 print(number_list)
 
-l = len(number_list)
 
-while i in number_list:
-    calc = numbers % 2
-    if calc == 0:
-        number_list.remove(i)
-    else:
-        pass
-print(number_list)
-
-#while numbers % 2 != 0:
-    #number_list.remove(numbers)
-    #print(number_list)
