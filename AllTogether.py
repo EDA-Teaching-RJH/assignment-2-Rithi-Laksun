cc0dd0999fad4ff2076293c934421dd4f20a3c51
Rithi-Laksun <rl471@kent.ac.uk>
2024-10-26 13:45:15 +0000

    Used DRY principle and used def to separate

--- AllTogether.py
+++ AllTogether.py
@@ -1,11 +1,21 @@
 ### Part Four -- your code goes here. 
-import random 
-
+import random
 number_list = []
-n = 10 
-for i in range(n):
-    numbers = random.randint(1,100)
+init_number_range = 10
+first_rand_num = 1
+last_rand_num = 100
+
+def init_random_num():
+  for i in range(init_number_range):
+    numbers = random.randint(first_rand_num,last_rand_num)
     number_list.append(numbers)
-print(number_list)
+  return number_list
 
+def remove_even_number(number_list, i_number):
+  while(i_number  < len(number_list)):
+    result = [ev for ev in number_list if ev %2 !=0]
+    return result
+  exit()
 
+init_random_num()
+print(remove_even_number(number_list,0))
